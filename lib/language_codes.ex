6800eaa04e94168d3180157dd49b15e5c84e67a2
Hugo Morgan <hpjm37@gmail.com>
2021-02-04 15:37:00 +0000

    read in file at compile time

--- lib/language_codes.ex
+++ lib/language_codes.ex
@@ -2,4 +2,7 @@ defmodule LanguageCodes do
   @moduledoc """
   Documentation for `LanguageCodes`.
   """
+
+  @external_resource codes_path = Path.join([__DIR__, "codes.json"])
+  @codes codes_path |> File.read!() |> Jason.decode!()
 end
