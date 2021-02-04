ab2a75bf9d5e0152d497f9517a424108482c3f11
Hugo Morgan <hpjm37@gmail.com>
2021-02-04 17:05:49 +0000

    add using block to inject from_code/1 and from_name/1 and set up language structs

--- lib/language_codes.ex
+++ lib/language_codes.ex
@@ -1,8 +1,42 @@
 defmodule LanguageCodes do
   @moduledoc """
-  Documentation for `LanguageCodes`.
+  Allows querying of ISO codes for language name and codes.
   """
 
   @external_resource codes_path = Path.join([__DIR__, "codes.json"])
   @codes codes_path |> File.read!() |> Jason.decode!()
+
+  defmodule Language do
+    defstruct [:code, :name, :native, :rtl?]
+  end
+
+  defmacro __using__(_) do
+    ast =
+      for %{"code" => code, "name" => name, "native" => native} = language_code <- @codes do
+        rtl? = Map.has_key?(language_code, "rtl")
+        language_code = %Language{name: name, code: code, rtl?: rtl?, native: native}
+
+        quote do
+          def from_code(unquote(code |> String.to_atom())) do
+            unquote(Macro.escape(language_code))
+          end
+
+          def from_name(unquote(name)) do
+            unquote(Macro.escape(language_code))
+          end
+        end
+      end
+
+    quote do
+      unquote(ast)
+
+      def from_code(_) do
+        {:error, :invalid_code}
+      end
+
+      def from_name(_) do
+        {:error, :invalid_name}
+      end
+    end
+  end
 end
--- test/language_codes_test.exs
+++ test/language_codes_test.exs
@@ -1,4 +1,30 @@
 defmodule LanguageCodesTest do
   use ExUnit.Case
   doctest LanguageCodes
+  alias LanguageCodes.Language
+
+  defmodule ExampleModule do
+    use LanguageCodes
+  end
+
+  describe "from_code/1" do
+    test "it has compiled options for codes" do
+      vietnamese = %Language{name: "Vietnamese", code: "vi", native: "Tiếng Việt", rtl?: false}
+      assert ExampleModule.from_code(:vi) == vietnamese
+    end
+
+    test "it handles non-existent matches" do
+      assert ExampleModule.from_name(:nothing) == {:error, :invalid_name}
+    end
+  end
+
+  describe "from_name/1" do
+    test "it has compiled options for names" do
+      assert %Language{code: "it"} = ExampleModule.from_name("Italian")
+    end
+
+    test "it handles non-existent matches" do
+      assert ExampleModule.from_name(:nothing) == {:error, :invalid_name}
+    end
+  end
 end
