17c080dcbf9d73e782e048c759fbbcf6cd442b36
Hugo Morgan <hpjm37@gmail.com>
2021-02-04 15:35:44 +0000

    remove boilerplate

--- lib/language_codes.ex
+++ lib/language_codes.ex
@@ -2,17 +2,4 @@ defmodule LanguageCodes do
   @moduledoc """
   Documentation for `LanguageCodes`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> LanguageCodes.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/language_codes_test.exs
+++ test/language_codes_test.exs
@@ -1,8 +1,4 @@
 defmodule LanguageCodesTest do
   use ExUnit.Case
   doctest LanguageCodes
-
-  test "greets the world" do
-    assert LanguageCodes.hello() == :world
-  end
 end
